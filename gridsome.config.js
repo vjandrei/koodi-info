fd31c02d80506fd1d6e7e75702ab6914d601f6a9
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2019-12-17 15:49:12 +0200

    some confs

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Digikokki Andreas',
+  siteDescription: "Suunnittelijasta koodariksi Suomeksi! Tämä on minun eli Andreasksen henkilökohtainen sivu joka sisältää Suomeksi koodi ja käyttöliittymä asioita erillaisissa formaateissa.",
+  titleTemplate: `%s | Digikokki`,
+  icon: 'src/favicon.png',
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'BlogPost',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+          author: {
+            typeName: 'Author',
+            create: true,
+          },
+        },
+      }
+    }
+  ]
+  
 }
