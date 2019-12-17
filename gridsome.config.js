17fc6d1d179e9fede45a6585353f80bc71759d4d
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2019-12-17 16:45:28 +0200

    modify config

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,18 +4,36 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   siteName: 'Digikokki Andreas',
   siteDescription: "Suunnittelijasta koodariksi Suomeksi! Tämä on minun eli Andreasksen henkilökohtainen sivu joka sisältää Suomeksi koodi ja käyttöliittymä asioita erillaisissa formaateissa.",
   titleTemplate: `%s | Digikokki`,
   icon: 'src/favicon.png',
+  siteUrl: 'https://youthful-wiles-bb2081.netlify.com/',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        ['gridsome-plugin-remark-shiki', {
+          theme: 'min-light'
+        }]
+      ]
+    }
+  },
 
   plugins: [
     {
       use: '@gridsome/source-filesystem',
       options: {
         path: 'content/posts/**/*.md',
-        typeName: 'BlogPost',
+        typeName: 'Post',
         refs: {
           tags: {
             typeName: 'Tag',
@@ -26,8 +44,82 @@ module.exports = {
             create: true,
           },
         },
-      }
-    }
-  ]
-  
+      },
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-135446199-1',
+      },
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      },
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Bleda, a Gridsome blog starter',
+          feed_url: 'https://gridsome-starter-bleda.netlify.com/feed.xml',
+          site_url: 'https://gridsome-starter-bleda.netlify.com',
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.description,
+          url: 'https://gridsome-starter-bleda.netlify.com/' + node.slug,
+          author: node.author,
+          date: node.date,
+        }),
+        output: {
+          dir: './static',
+          name: 'feed.xml',
+        },
+      },
+    },
+  ],
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id',
+    Author: '/author/:id',
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(...[
+          require('postcss-import'),
+          require('postcss-nested'),
+          require('tailwindcss'),
+        ])
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[
+            require('@fullhuman/postcss-purgecss')({
+              content: [
+                'src/assets/**/*.css',
+                'src/**/*.vue',
+                'src/**/*.js'
+              ],
+              extractors: [
+                {
+                  extractor: TailwindExtractor,
+                  extensions: ['css', 'vue', 'js']
+                }
+              ],
+              whitelistPatterns: [/shiki/]
+            }),
+          ])
+        }
+
+        return options
+      })
+  },
 }
