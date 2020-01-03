0265933d160c37b909e251a913fe2d1b2f9a3022
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-01-03 14:02:43 +0200

    visual update

--- src/templates/koodiInfo.vue
+++ src/templates/koodiInfo.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-  	<h1 v-html="$page.posts.entry.title" />
-  	<div v-html="$page.posts.entry.summaryContent.content" />
-    <div v-html="$page.posts.entry.bodyContent.content" />
+    <SiteHeader class="xl:flex-shrink-0 border-b border-gray-200"/> 
+      <section class="container mx-auto px-4 py-3 mt-16">
+        <h1 class="text-4xl font-bold my-4" v-html="$page.posts.entry.title" />
+        <g-image src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80" width="500" height="500" fit="contain"/>
+
+        <div v-html="$page.posts.entry.summaryContent.content" />
+        <div v-html="$page.posts.entry.bodyContent.content" />
+      </section>
   </div>
 </template>
 
@@ -28,7 +33,10 @@ query Craft ($id: [Int]){
 </page-query>
 
 <script>
+import SiteHeader from '~/components/SiteHeader.vue'
 export default {
-  
+   components: {
+    SiteHeader,
+  },
 };
 </script>
