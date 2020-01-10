59b209ce8778e3e305d1b6d816c2e73c039093a8
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-01-10 11:16:58 +0200

    highlight.js

--- src/templates/koodiInfo.vue
+++ src/templates/koodiInfo.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <SiteHeader class="xl:flex-shrink-0 border-b border-gray-200"/> 
-      <section class="markdown container mx-auto px-4 py-3">
-        <div class="w-3/4 mx-auto">
-          <h1 class="text-4xl font-bold my-4" v-html="$page.posts.entry.title" />
-          <g-image src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80" width="500" height="500" fit="contain"/>
-          <div v-html="$page.posts.entry.summaryContent.content" />
-          <div class="markdown" v-html="compiledMarkdown"></div>
-        </div>
-      </section>
+    <section class="markdown container mx-auto px-4 py-3">
+      <div class="w-3/4 mx-auto">
+        <h1 class="text-4xl font-bold my-4" v-html="$page.posts.entry.title" />
+        <div v-html="$page.posts.entry.summaryContent.content" />
+        <div class="markdown" v-html="compiledMarkdown"></div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -22,12 +21,12 @@ export default {
   },
   computed: {
     compiledMarkdown: function () {
-      return marked(this.$page.posts.entry.bodyContent, { 
-        sanitize: true,
-        highlight: function(code) {
+      marked.setOptions({
+        highlight: (code, lang) => {
           return require('highlight.js').highlightAuto(code).value;
-        },
+        }
       })
+      return marked(this.$page.posts.entry.bodyContent)
     }
   }
 };
