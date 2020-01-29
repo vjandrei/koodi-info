e0aa524cbe7b20069888fc88146fb4f79d258369
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-01-29 11:04:39 +0200

    ui update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "~/assets/css/main.css"
 export default function (Vue, { router, head, isClient }) {
   // Add a html attributes
   head.htmlAttrs = { lang: 'fi', class: 'h-full' }
-  head.bodyAttrs = { class: 'bg-gray-100 text-gray-900 tracking-wider leading-normal antialiased font-sans min-h-screen xl:flex xl:flex-col xl:h-screen' }
+  head.bodyAttrs = { class: 'bg-gray-100 text-gray-900 leading-normal antialiased font-sans min-h-screen xl:flex xl:flex-col xl:h-screen m-0' }
   // Add a meta tag
   head.meta.push({
     name: 'keywords',
