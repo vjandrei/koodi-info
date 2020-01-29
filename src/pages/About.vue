c7f3b2536d237811efae9657ddebac88951d4473
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-01-29 14:05:07 +0200

    ui updates

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,35 @@
 <template>
-  <Layout class="bg-white">
+  <Layout class="h-full">
     <section class="container mx-auto px-4 py-3">
-    <div class="sm:w-3/5 w-full mx-auto">
+    <div class="sm:w-3/4 w-full mx-auto">
       <div class="text-center sm:mt-16 mt-8">
-        <span class="text-xs font-bold uppercase text-teal-500 mb-2 block">Tietoja tästä sivusta</span>
-        <h1 class="sm:text-5xl text-2xl font-bold leading-tight mt-0 mb-4">Minäpä kerron</h1>
+        <span class="text-xs font-bold uppercase text-teal-500 mb-2 block">Tietoja sivusta</span>
+        <h1 class="sm:text-5xl text-2xl font-bold leading-tight mt-0 mb-4">Muutamalla lauseella Koodi.info</h1>
       </div>
-      <p class="my-6 sm:text-2xl text-lg leading-relaxed tracking-wide">
-      ...
+
+      <p class="my-6 sm:text-2xl text-lg leading-relaxed tracking-wide text-teal-900">
+      Koodi.info on blogi joka yhdistelee tekstiä, audiota ja kuvakaappausta ohjelmistoalan liittyvistä aiheista.
+      Aiheina ovat mm. webin teknlogiat, käyttöliittymäsuunnittelu, arkkitehtuurit, muut ohjelmisto teknologiat sekä avointa keskustelua ohjelmistoalasta.
       </p>
+
+      <p class="sm:text-3xl text-xl tracking-wider text-teal-800 text-center my-16">
+      Kiva kun piipahdit tällä sivulla!
+      Jos sinulla on jotain kysyttävää ole yhteydessä <a href="https://twitter.com/vj_andrei" target="blank">Twitterin</a> kautta.
+      <span>💌</span>
+      </p>
+
+      <h3 class="sm:text-4xl text-xl text-center text-teal-500 font-semibold">Koodi.info takana häärää</h3>
+      <g-image class="mx-auto my-8" src="~/assets/andreas.jpg" width="500"/>
+      <p class="sm:text-2xl text-xl tracking-normal text-teal-900 my-16"> 
+      Moikka! 👋🏻 Olen Andreas Koutsoukos tämän sivuston kehittäjä sekä isäntä. Osaamisen jakaminen on minulle ollut tärkeätä. 
+      Päätin siksi toteuttaa kanavan jossa voin lisää vuorovaikutusta ja sitä kautta jakaa osaamista. Olen myös itse alanvaihtaja 👨🏻‍🍳 ja 
+      oppinut asioita paljon itse kokeilemalla, se on yksi syy miksi osaaminen on laaja. Päivätyökseni työskentelen käyttöliittymäsuunnittelun 👨🏻‍🎨 parissa.
+      </p>
+
+      <h3 class="bg-teal-100 text-teal-500 p-4 text-xl text-center">
+        Kiinnostaako sisältöyhteistyötä? <a href="https://twitter.com/vj_andrei" class="block text-teal-900">Ota yhteyttä täällä</a> 
+      </h3>
+
     </div> 
     </section>
   </Layout>
@@ -17,7 +38,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'Tietoa Koodi.info'
   }
 }
 </script>
