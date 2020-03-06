0aab28cd8dd0b6ce84ef670cbd220c657e269e58
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-06 09:37:16 +0200

    prettify

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,67 @@
 <template>
-  <article itemscope="itemscope" itemtype="http://schema.org/CreativeWork" class="my-4 mx-4 bg-white py-8 px-8">
+  <article
+    itemscope="itemscope"
+    itemtype="http://schema.org/CreativeWork"
+    class="my-4 mx-4 bg-white py-8 px-8"
+  >
     <g-link :to="`${post.uri}/`" class="flex items-center">
       <div class="max-w-md">
-        <h5 class="text-xs font-bold tracking-widest uppercase mb-2 text-teal-700">{{post.postSubjects[0].title}}</h5>
-        <h3 itemprop="name" class="text-2xl font-semibold font-serif text-brand-dark leading-tight">{{ post.postTitle }}</h3>
+        <h5
+          class="text-xs font-bold tracking-widest uppercase mb-2 text-teal-700"
+        >{{post.postSubjects[0].title}}</h5>
+        <h3
+          itemprop="name"
+          class="text-2xl font-semibold font-serif text-brand-dark leading-tight"
+        >{{ post.postTitle }}</h3>
         <div class="flex items-center my-2">
-          <svg aria-hidden="true" data-prefix="fas" data-icon="calendar-alt" class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"/></svg>
+          <svg
+            aria-hidden="true"
+            data-prefix="fas"
+            data-icon="calendar-alt"
+            class="w-4 h-4 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill="currentColor"
+              d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"
+            />
+          </svg>
           <time class="text-sm font-semibold">{{this.postDate}}</time>
         </div>
         <div class="text-sm text-gray-800 mt-2" v-html="excerpt(post, 150, ' ...')"></div>
         <div class="flex align-middle text-sm text-gray-800 font-semibold mt-4">
           <span class="mr-2 font-normal text-xs xl:text-sm text-gray-800">Artikkeli sisältää:</span>
-          <div class="flex items-center mr-4" v-if="post.pageSoundContent[0]"> 
-            <svg aria-hidden="true" data-prefix="fas" data-icon="headphones" class="w-4 h-4 mr-2 fill-current text-brand-neutral" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 32C114.52 32 0 146.496 0 288v48a32 32 0 0017.689 28.622l14.383 7.191C34.083 431.903 83.421 480 144 480h24c13.255 0 24-10.745 24-24V280c0-13.255-10.745-24-24-24h-24c-31.342 0-59.671 12.879-80 33.627V288c0-105.869 86.131-192 192-192s192 86.131 192 192v1.627C427.671 268.879 399.342 256 368 256h-24c-13.255 0-24 10.745-24 24v176c0 13.255 10.745 24 24 24h24c60.579 0 109.917-48.098 111.928-108.187l14.382-7.191A32 32 0 00512 336v-48c0-141.479-114.496-256-256-256z"/></svg>
+          <div class="flex items-center mr-4" v-if="post.pageSoundContent[0]">
+            <svg
+              aria-hidden="true"
+              data-prefix="fas"
+              data-icon="headphones"
+              class="w-4 h-4 mr-2 fill-current text-brand-neutral"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M256 32C114.52 32 0 146.496 0 288v48a32 32 0 0017.689 28.622l14.383 7.191C34.083 431.903 83.421 480 144 480h24c13.255 0 24-10.745 24-24V280c0-13.255-10.745-24-24-24h-24c-31.342 0-59.671 12.879-80 33.627V288c0-105.869 86.131-192 192-192s192 86.131 192 192v1.627C427.671 268.879 399.342 256 368 256h-24c-13.255 0-24 10.745-24 24v176c0 13.255 10.745 24 24 24h24c60.579 0 109.917-48.098 111.928-108.187l14.382-7.191A32 32 0 00512 336v-48c0-141.479-114.496-256-256-256z"
+              />
+            </svg>
             <span>Audion</span>
           </div>
           <div class="flex items-center" v-if="post.pageVideo">
-            <svg aria-hidden="true" data-prefix="fab" data-icon="youtube" class="w-4 h-4 mr-2 fill-current text-brand-neutral" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>
+            <svg
+              aria-hidden="true"
+              data-prefix="fab"
+              data-icon="youtube"
+              class="w-4 h-4 mr-2 fill-current text-brand-neutral"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="currentColor"
+                d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+              />
+            </svg>
             <span>Videon</span>
           </div>
         </div>
@@ -27,29 +72,32 @@
 
 <script>
 export default {
-  name: 'Card',
-  props: ['post'],
-  data () {
+  name: "Card",
+  props: ["post"],
+  data() {
     return {
-      postDate: ''
-    }
+      postDate: ""
+    };
   },
   methods: {
     excerpt(post, length, clamp) {
       if (post.excerpt) {
-        return post.excerpt
+        return post.excerpt;
       }
 
-      length = length || 280
-      clamp = clamp || ' ...'
-      let text = post.pageSummaryContent.content.replace(/<pre(.|\n)*?<\/pre>/gm, '').replace(/<[^>]+>/gm, '')
+      length = length || 280;
+      clamp = clamp || " ...";
+      let text = post.pageSummaryContent.content
+        .replace(/<pre(.|\n)*?<\/pre>/gm, "")
+        .replace(/<[^>]+>/gm, "");
 
-      return text.length > length ? `${ text.slice(0, length)}${clamp}` : text
-
-    },
+      return text.length > length ? `${text.slice(0, length)}${clamp}` : text;
+    }
   },
   mounted() {
-    this.postDate = this.$moment.unix(Number(this.post.postDate)).format("DD.MM.YYYY")
+    this.postDate = this.$moment
+      .unix(Number(this.post.postDate))
+      .format("DD.MM.YYYY");
   }
-}
+};
 </script>
--- src/templates/koodiInfo.vue
+++ src/templates/koodiInfo.vue
@@ -1,46 +1,66 @@
 <template>
   <div>
-    <SiteHeader class="xl:flex-shrink-0 border-b border-gray-200"/> 
-    <section class="container mx-auto ">
-      <div class="w-full sm:w-auto  p-8 xl:px-56 xl:py-16 mx-auto border-b border-gray-200 bg-white">
-      
+    <SiteHeader class="xl:flex-shrink-0 border-b border-gray-200" />
+    <section class="container mx-auto">
+      <div class="w-full sm:w-auto p-8 xl:px-56 xl:py-16 mx-auto border-b border-gray-200 bg-white">
         <div class="text-center sm:mt-16 xl:mt-8">
-          <span class="text-xs font-bold uppercase text-teal-700 mb-2 block">{{$page.post.entry.postSubjects[0].title}}</span>
-          <h1 class="sm:text-4xl text-2xl font-bold leading-tight mt-0 mb-4 font-serif font-black" v-html="$page.post.entry.title" />
+          <span
+            class="text-xs font-bold uppercase text-teal-700 mb-2 block"
+          >{{$page.post.entry.postSubjects[0].title}}</span>
+          <h1
+            class="sm:text-4xl text-2xl font-bold leading-tight mt-0 mb-4 font-serif font-black"
+            v-html="$page.post.entry.title"
+          />
         </div>
-        <div class="flex sm:w-3/4 w-full mx-auto flex-row justify-center items-center text-center my-6 sm:text-base text-xs">
+        <div
+          class="flex sm:w-3/4 w-full mx-auto flex-row justify-center items-center text-center my-6 sm:text-base text-xs"
+        >
           <div class="flex-1 px-2">
             <p v-html="$page.post.entry.author.name" />
           </div>
-          <div class="flex-1 px-2 border-r-2 border-l-2  border-teal-500">
+          <div class="flex-1 px-2 border-r-2 border-l-2 border-teal-500">
             <time>{{this.postDate}}</time>
           </div>
           <div class="flex-1 px-2"></div>
         </div>
-     
-        <div class="my-6 sm:text-2xl text-lg leading-snug my-12 font-serif text-center" v-html="$page.post.entry.pageSummaryContent.content" />
+
+        <div
+          class="my-6 sm:text-2xl text-lg leading-snug my-12 font-serif text-center"
+          v-html="$page.post.entry.pageSummaryContent.content"
+        />
         <div class="my-6 w-full mx-auto mb-20">
           <div v-for="audio in podcastAudioFile" :key="audio.id">
             <vue-audio :file="audio.url"></vue-audio>
           </div>
-        </div>    
+        </div>
         <div class="markdown" v-html="compiledMarkdown"></div>
-        
+
         <div v-if="$page.post.entry.pageVideo">
           <div class="markdown">
             <h2>Video</h2>
-            <iframe type="text/html" width="100%" height="400" 
-            :src="'https://www.youtube.com/embed/'+ $page.post.entry.pageVideo +'?autoplay=0&origin=http://koodi.info'" frameborder="0"></iframe>
+            <iframe
+              type="text/html"
+              width="100%"
+              height="400"
+              :src="'https://www.youtube.com/embed/'+ $page.post.entry.pageVideo +'?autoplay=0&origin=http://koodi.info'"
+              frameborder="0"
+            ></iframe>
           </div>
         </div>
 
         <div v-if="$page.post.entry.postCodeLearn">
-            <div class="markdown">
-              <h2>Live koodi</h2>
-              <iframe width="100%" height="700" :src="$page.post.entry.postCodeLearn" frameborder="0" allowfullscreen></iframe>
-            </div>
+          <div class="markdown">
+            <h2>Live koodi</h2>
+            <iframe
+              width="100%"
+              height="700"
+              :src="$page.post.entry.postCodeLearn"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+          </div>
         </div>
-        <div v-if="$page.post.entry.postLinkList.length == 0">
+        <div v-if="$page.post.entry.postLinkList">
           <div class="markdown">
             <h2>Linkkit</h2>
           </div>
@@ -49,70 +69,96 @@
               <a v-bind:href="link.code">{{link.code}}</a>
             </li>
             <li>
-               <a v-bind:href="link.design">{{link.design}}</a>
+              <a v-bind:href="link.design">{{link.design}}</a>
             </li>
             <li>
-               <a v-bind:href="link.wow">{{link.wow}}</a>
+              <a v-bind:href="link.wow">{{link.wow}}</a>
             </li>
           </ul>
         </div>
-        
-
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import marked from 'marked'
-import SiteHeader from '~/components/SiteHeader.vue'
-import VueAudio from '~/components/Audio.vue'
+import marked from "marked";
+import SiteHeader from "~/components/SiteHeader.vue";
+import VueAudio from "~/components/Audio.vue";
 
 export default {
-  data (){
+  data() {
     return {
-      podcastAudio: '',
-      postDate: ''
-    }
+      podcastAudio: "",
+      postDate: ""
+    };
   },
-  metaInfo () {
+  metaInfo() {
     return {
       title: this.$page.post.entry.pageSeoContent.title,
       meta: [
-        { name: 'description', content: this.$page.post.entry.pageSeoContent.description },
-        { property: "og:type", content: 'website' },
-        { property: "og:title", content: this.$page.post.entry.pageSeoContent.social.facebook.title },
-        { property: "og:description", content: this.$page.post.entry.pageSeoContent.social.facebook.description },
-        { property: "og:url", content:  "https://koodi.info/" + this.$page.post.entry.uri },
-        { property: "og:image",  content: this.$page.post.entry.pageSeoContent.social.facebook.image.url },
-        { name: "twitter:card", content: "Summary"  },
-        { name: "twitter:title", content: this.$page.post.entry.pageSeoContent.social.twitter.title },
-        { name: "twitter:description", content: this.$page.post.entry.pageSeoContent.description },
+        {
+          name: "description",
+          content: this.$page.post.entry.pageSeoContent.description
+        },
+        { property: "og:type", content: "website" },
+        {
+          property: "og:title",
+          content: this.$page.post.entry.pageSeoContent.social.facebook.title
+        },
+        {
+          property: "og:description",
+          content: this.$page.post.entry.pageSeoContent.social.facebook
+            .description
+        },
+        {
+          property: "og:url",
+          content: "https://koodi.info/" + this.$page.post.entry.uri
+        },
+        {
+          property: "og:image",
+          content: this.$page.post.entry.pageSeoContent.social.facebook.image
+            .url
+        },
+        { name: "twitter:card", content: "Summary" },
+        {
+          name: "twitter:title",
+          content: this.$page.post.entry.pageSeoContent.social.twitter.title
+        },
+        {
+          name: "twitter:description",
+          content: this.$page.post.entry.pageSeoContent.description
+        },
         { name: "twitter:site", content: "@vj_andrei" },
         { name: "twitter:creator", content: "@vj_andrei" },
-        { name: "twitter:image", content: this.$page.post.entry.pageSeoContent.social.twitter.image.url },
-      ],
-    }
+        {
+          name: "twitter:image",
+          content: this.$page.post.entry.pageSeoContent.social.twitter.image.url
+        }
+      ]
+    };
   },
   components: {
     SiteHeader,
-   'vue-audio': VueAudio
+    "vue-audio": VueAudio
   },
   computed: {
-    compiledMarkdown: function () {
+    compiledMarkdown: function() {
       marked.setOptions({
         highlight: (code, lang) => {
-          return require('highlight.js').highlightAuto(code).value;
+          return require("highlight.js").highlightAuto(code).value;
         }
-      })
-      return marked(this.$page.post.entry.pageContent)
+      });
+      return marked(this.$page.post.entry.pageContent);
     },
-    podcastAudioFile: function () {
-      return this.podcastAudio = this.$page.post.entry.pageSoundContent
-    },
+    podcastAudioFile: function() {
+      return (this.podcastAudio = this.$page.post.entry.pageSoundContent);
+    }
   },
   mounted() {
-    this.postDate = this.$moment.unix(Number(this.$page.post.entry.postDate)).format("DD.MM.YYYY")
+    this.postDate = this.$moment
+      .unix(Number(this.$page.post.entry.postDate))
+      .format("DD.MM.YYYY");
   }
 };
 </script>
