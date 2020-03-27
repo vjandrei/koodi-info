80784de72238be74a388b117b2173a17b4011512
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-27 06:47:13 +0200

    update

--- src/components/PodcastAd.vue
+++ src/components/PodcastAd.vue
@@ -10,16 +10,25 @@
     <div class="mt-4 md:ml-6">
       <div
         class="text-xs font-bold tracking-widest uppercase mb-2 text-teal-700"
+<<<<<<< Updated upstream
       >
         🎙 Ystävyys podcatit
       </div>
+=======
+      >🎙 Ystävyys podcatit</div>
+>>>>>>> Stashed changes
       <g-link
         :to="`${podcast.rss}`"
         target="_black"
         class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+<<<<<<< Updated upstream
         >{{ podcast.title }}</g-link
       >
       <p class="mt-2 text-gray-600 text-sm">{{ podcast.about }}</p>
+=======
+      >{{podcast.title}}</g-link>
+      <p class="mt-2 text-gray-600 text-sm">{{podcast.about}}</p>
+>>>>>>> Stashed changes
     </div>
   </div>
 </template>
