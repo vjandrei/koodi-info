6470b0e35e8b7f01fdaf51dd700da63fe657afce
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-04-16 09:02:14 +0300

    hide notes

--- src/components/TabNotes.vue
+++ src/components/TabNotes.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="m-4">
+    Tulossa
     <ul>
-      <li v-for="note in $page.notes.edges" :key="note.node.id">
-        <p class="text-1xl md:text-xl font-semibold font-serif text-brand-dark leading-tight">
-          <span v-html="note.node.name"></span>
-        </p>
-      </li>
+      <li v-for="note in $page.notes.edges" :key="note.node.id"></li>
     </ul>
   </div>
 </template>
