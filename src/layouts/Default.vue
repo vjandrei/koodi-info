6b3b833e8badb7107578f0963854978a5425394a
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-12-08 22:05:30 +0200

    new gridsome and tailwind

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div class="container mx-auto">
     <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -9,7 +9,7 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -22,20 +22,6 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
 .header {
   display: flex;
   justify-content: space-between;
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,53 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="min-h-screen flex flex-col justify-start sm:py-12">
+      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div
+          class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"
+        ></div>
+        <div
+          class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"
+        >
+          <div class="max-w-md mx-auto">
+            <div>
+              <img src="/img/logo.svg" class="h-7 sm:h-8" />
+            </div>
+            <div class="divide-y divide-gray-200">
+              <div
+                class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+              >
+                <p>
+                  An advanced online playground for Tailwind CSS, including
+                  support for things like:
+                </p>
+              </div>
+              <div
+                class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7"
+              >
+                <p>Want to dig deeper into Tailwind?</p>
+                <p>
+                  <a
+                    href="https://tailwindcss.com/docs"
+                    class="text-cyan-600 hover:text-cyan-700"
+                  >
+                    Read the docs &rarr;
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: 'Hello, world!',
+  },
+};
 </script>
 
 <style>
